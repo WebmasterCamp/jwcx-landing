e03b2d5c11bb289776598a2b38af535524aaa639
Rachasak Ragkamnerd <id513128@gmail.com>
2018-03-28 22:56:16 +0700

    Add Q&A switch JS

--- assets/js/script-qa.js
+++ assets/js/script-qa.js
@@ -1,3 +1,17 @@
 (function() {
 	new SimpleBar(document.getElementById('qa-questions-list'), { autoHide: false });
+	$(".qa-main-question").click(function(e){
+		e.preventDefault();
+		if($(this).hasClass("active"))
+			return;
+
+		let index = $("#qa-questions-list .qa-main-question").index(this);
+		console.log("Index", index);
+
+		if(index === -1)
+			return;
+
+		$(".qa-main-question.active, .qa-main-answer-item.active").removeClass("active");
+		$(`.qa-main-question:eq(${index}), .qa-main-answer-item:eq(${index})`).addClass("active");
+	})
 })();
